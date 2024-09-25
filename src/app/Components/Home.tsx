974826b7a0a98fc195951f7c9ccad9417d5aea16
Nacho Ledesma <nacho.ledesma@example.com>
2024-09-25 12:45:17 -0300

    Add new styles

--- src/app/Components/Home.tsx
+++ src/app/Components/Home.tsx
@@ -5,15 +5,16 @@ import { motion } from "framer-motion";
 const Home: React.FC = () => {
   return (
     <div className="bg-gradient-to-br from-gray-900 via-gray-800 to-black min-h-screen p-12 text-red-200 flex flex-col lg:flex-row justify-center items-center rounded-lg">
-
       <motion.div
         className="w-full lg:w-1/2 text-center lg:text-left"
         initial={{ opacity: 0, x: -100 }}
-        animate={{ opacity: 1, x: 0 }} 
-        transition={{ duration: 0.8 }} 
+        animate={{ opacity: 1, x: 0 }}
+        transition={{ duration: 0.8 }}
       >
         <h1 className="text-7xl font-bold mb-5">Ledesma Ignacio</h1>
-        <h2 className="text-4xl font-bold mb-5">Desarrollador Web Full Stack</h2>
+        <h2 className="text-4xl font-bold mb-5">
+          Desarrollador Web Full Stack
+        </h2>
         <p className="text-2xl text-red-100 text-justify lg:text-left">
           Soy un apasionado programador web con enfoque en el desarrollo de
           aplicaciones modernas.
@@ -22,16 +23,16 @@ const Home: React.FC = () => {
 
       <motion.div
         className="w-full lg:w-1/2 flex justify-center mt-8 lg:mt-0 lg:ml-8"
-        initial={{ opacity: 0, x: 100 }} 
-        animate={{ opacity: 1, x: 0 }} 
-        transition={{ duration: 0.8 }} 
+        initial={{ opacity: 0, x: 100 }}
+        animate={{ opacity: 1, x: 0 }}
+        transition={{ duration: 0.8 }}
       >
         <div className="w-80 h-[26rem] overflow-hidden rounded-full border-2 border-red-100 shadow-lg">
           <Image
             src="https://avatars.githubusercontent.com/u/118952823?v=4"
             alt="Ledesma Ignacio"
-            width={200}
-            height={600}
+            width={700}
+            height={800}
             className="object-cover w-full h-full"
           />
         </div>
