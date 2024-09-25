79136eb015b81815695a198f4abd14c6ff6731b6
Nacho Ledesma <nacho.ledesma@example.com>
2024-09-25 13:31:09 -0300

    add some images

--- src/app/Components/Certificates.tsx
+++ src/app/Components/Certificates.tsx
@@ -2,8 +2,8 @@ import React from 'react';
 
 const Certificates: React.FC = () => {
   return (
-    <div className="bg-gradient-to-br from-gray-900 via-gray-800 to-black min-h-screen p-12 text-gray-300 flex flex-col lg:flex-row justify-center items-center rounded-lg">
-      <h2 className="text-2xl font-bold">Certificates</h2>
+    <div className="bg-gradient-to-br from-gray-900 via-gray-800 to-black min-h-screen p-12 text-gray-300 flex flex-col justify-center items-center rounded-lg">
+      <h2 className="text-4xl font-bold text-red-300 text-center py-1">Certificados</h2>
       <p className="mt-4 text-gray-600">Here are my certificates.</p>
     </div>
   );
