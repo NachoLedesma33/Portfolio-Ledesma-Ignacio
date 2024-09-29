c574beabd210029c98b17157e756c9da9cfe57b3
Nacho Ledesma <nacho.ledesma@example.com>
2024-09-29 08:42:12 -0300

    change and fix file next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -2,15 +2,6 @@
 const nextConfig = {
   images: {
     domains: ["avatars.githubusercontent.com", "localhost"],
-
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "avatars.githubusercontent.com",
-        port: "3000",
-        pathname: "/u/**",
-      },
-    ],
   },
 };
 
